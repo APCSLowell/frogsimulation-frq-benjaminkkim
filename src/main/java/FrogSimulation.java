b452a543fa10e8e4d950242fcf245edc336d8cf3
benjaminkkim <bekim@s.sfusd.edu>
2025-04-24 22:17:02 -0700

    Update FrogSimulation.java

--- src/main/java/FrogSimulation.java
+++ src/main/java/FrogSimulation.java
@@ -9,16 +9,18 @@ public class FrogSimulation
 		maxHops = numHops;
 		maxHopsHolder = numHops;  //ignore this, it's used for testing
 	}
-	
 	//private int hopDistance() 
 	//implementation is below, but not important
-	
+
+	//runs a simulation of the new FrogSimulation object
 	public boolean simulate() { 
 		int dist = 0;
 		int hops = 0;
+		//while the frog still has hops left, add it to the hopDistance, increment hops to count them
 		while(hops < maxHops) {
 			dist += hopDistance();
 			hops++;
+			//return false if the frog is in negative distance, return true if it passed the goal
 			if(dist < 0) {
 				return false;
 			}
@@ -26,16 +28,21 @@ public class FrogSimulation
 				return true;
 			}
 		}
+		//otherwise return false;
 		return false;
 	}
-	
+
+	//counts the proportion of successful runs (i think)
 	public double runSimulations(int num) { 
+		//double so it can be divided later
 		double successfulRuns = 0;
+		//just literally adds the successful runs
 		for(int i = 0; i < num; i++) {
 			if(simulate() == true) {
 				successfulRuns++;
 			}
 		}
+		//the proportion part
 		return successfulRuns/num;
 	}
 	
